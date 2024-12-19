c7910bd548c39073f0d9110d5ef01f096a2a2c8b
AutisticGoat <smhkotry@gmail.com>
2024-12-19 11:19:06 -0700

    Fixed Repentance+ check

--- main.lua
+++ main.lua
@@ -7,9 +7,9 @@ OmoriMod = RegisterMod("Omori", 1)
 local font = Font()
 font:Load("font/pftempestasevencondensed.fnt")
 
-if not REPENTANCE_PLUS then
+if REPENTANCE_PLUS then
     OmoriMod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
-        local text = "Repentace+ is unsuported"
+        local text = "Repentace+ is unsuported at the moment"
         local text2 = "Downgrade to Repentance"
         font:DrawStringScaledUTF8(text, Isaac.GetScreenWidth()/1.1 - font:GetStringWidthUTF8(text)/2, Isaac.GetScreenHeight()/1.2, 1, 1, KColor(2,.5,.5,1), 1, true )
         font:DrawStringScaledUTF8(text2, Isaac.GetScreenWidth()/1.1 - font:GetStringWidthUTF8(text2)/2, Isaac.GetScreenHeight()/1.2 + 8, 1, 1, KColor(2,.5,.5,1), 1, true )
@@ -27,14 +27,8 @@ if not REPENTOGON then
     return
 end
 
--- local myFolder = "resources.scripts.omori_library_of_isaac"
--- local LOCAL_TSIL = require(myFolder .. ".TSIL")
--- LOCAL_TSIL.Init(myFolder)
-
 OmoriMod.saveManager = include("resources.scripts.misc.save_manager")
 OmoriMod.saveManager.Init(OmoriMod)
 
-
 include("include")
-
 include("resources/scripts/dss/deadseascrolls")
